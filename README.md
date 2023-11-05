679174e9b6cd16fde6e0304fbfc55538711b2dbe
niceaji <a.ji@daum.net>
2023-11-05 21:56:47 +0900

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Svelte + UnoCSS + Vite
+
+```
+$> npx degit niceaji/svelte-unocss-vite myapp
+$> cd myapp
+$> npm install
+$> npm run dev
+```
---
+++ src/App.svelte
@@ -0,0 +1,4 @@
+<div class="m-10">
+  <h1 class="bg-dark-300 p-6 text-white">hello! UnoCSS</h1>
+  <div class="i-mdi-alarm text-orange-400 text-8xl">icon</div>
+</div>
